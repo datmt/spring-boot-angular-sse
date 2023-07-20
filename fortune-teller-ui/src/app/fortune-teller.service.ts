219ae58aeb3ba50a6a5b78b71e300314d0aa12ca
Mạnh Đạt <dat.tm24@gmail.com>
2023-07-20 19:02:46 +0700

    implement subscriber id for user

--- fortune-teller-ui/src/app/fortune-teller.service.ts
+++ fortune-teller-ui/src/app/fortune-teller.service.ts
@@ -2,7 +2,8 @@ import {Injectable} from '@angular/core';
 import {Observable, shareReplay, Subject} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {SimpleResponse} from "./simple-response";
-
+//@ts-ignore
+import {v4 as uuid} from 'uuid';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,11 +12,14 @@ export class FortuneTellerService {
   private eventSource: EventSource | undefined;
   private sseDataSubject: Subject<string> = new Subject<string>();
   private static retryCount = 0;
+  private static readonly MAX_RETRIES = 5;
+  //generate unique id for each subscriber
+  private static subscriberId = uuid();
   constructor(private httpClient: HttpClient) {
   }
 
   private connectToSSE() {
-    this.eventSource = new EventSource('http://localhost:8080/teller/notify');
+    this.eventSource = new EventSource(`http://localhost:8080/teller/subscribe/${FortuneTellerService.subscriberId}`);
     console.log('creating event source');
     this.eventSource.onmessage = event => {
       console.log('received event', event)
@@ -24,7 +28,7 @@ export class FortuneTellerService {
 
     this.eventSource.onerror = error => {
       console.log('error', error);
-      if (FortuneTellerService.retryCount > 5) {
+      if (FortuneTellerService.retryCount > FortuneTellerService.MAX_RETRIES) {
         console.log('too many retries');
         this.sseDataSubject.error(error);
         this.eventSource!.close();
@@ -45,7 +49,7 @@ export class FortuneTellerService {
   }
 
   requestFortuneTeller(name: string): Observable<SimpleResponse> {
-    return this.httpClient.get<SimpleResponse>(`http://localhost:8080/teller/future/${name}`).pipe(shareReplay());
+    return this.httpClient.get<SimpleResponse>(`http://localhost:8080/teller/future/${name}/${FortuneTellerService.subscriberId}`).pipe(shareReplay());
   }
 
 }
